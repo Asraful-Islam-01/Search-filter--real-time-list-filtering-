681fb3c865a6c6083e5c57d1b8bb46ae2d7e89cb
Asraful-Islam-01 <asrafulislam.12967@gmail.com>
2025-11-30 18:13:42 +0600

    Some js code added to the code base

---
+++ script.js
@@ -0,0 +1,26 @@
+const items = [{
+    name: "JavaScript",
+    description:"Popular scripting language for web development"
+},
+{
+    name: "Python",
+    description: "Versatile language for data science and web apps"
+}, 
+{
+    name: "React",
+    description: "JavaScript library for building user interfaces"
+},
+{name: "Vue.js", description: "Progessive framework for building UIs"},
+{name: "Angular", description: "Platform for buliding web applications"},
+{
+    name: "Node.js",
+    description: "JavaScript runtime built on Chrome V8 engine"
+},
+{name: "TypeScript", description: "Typed superset of javaScript"},
+{name: "CSS", description: "Style sheet language for web design"},
+{name: "HTML", description: "Standard markup language for web pages"},
+{name: "SQL", description: "Language for managing relational database"},
+{name: "MongoDB", description: "NoSQL ,,, "},
+{}
+
+];
