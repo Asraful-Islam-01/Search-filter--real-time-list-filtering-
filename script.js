0a25fc8611249824c901b89c2142cc30d0b204e5
Asraful-Islam-01 <asrafulislam.12967@gmail.com>
2025-12-02 17:25:27 +0600

    some code added to the code base

--- script.js
+++ script.js
@@ -20,7 +20,44 @@ const items = [{
 {name: "CSS", description: "Style sheet language for web design"},
 {name: "HTML", description: "Standard markup language for web pages"},
 {name: "SQL", description: "Language for managing relational database"},
-{name: "MongoDB", description: "NoSQL ,,, "},
-{}
-
+{name: "MongoDB", description: "NoSQL database program"},
+{name: "express.js", description: "Web application framework for Node.js"}
 ];
+
+const searchInput = document.getElementById("searchInput");
+const itemList = document.getElementById("itemList");
+const resultsCount = document.getElementById("resultsCount");
+const clearBtn = document.getElementById("clearBtn");
+
+function highLightMatch(text, query) {
+    // if no query provided, return original text without highlighting 
+    if(!query) return text;
+
+    // Create a regular expression to find all  occurrences of the query (case-insensitive)
+    // 'gi' flags: g = global (find all matches), i = case-insensitive
+    const regex = new RegExp(query,"gi");
+
+    // Replace all matches with the same text wrapped in <mark> tags for highlighting
+    // $& represents the matched substring
+    return text.replace(regex, <mark>$&</mark>)
+}
+
+/**
+ * Filters and displays items based on search query 
+ * @param {string} query - The search term entered by the user 
+ */
+function filterItems(query) {
+    //Convert query to lowercase for case insesitive comparison
+    const searchTerm = query.toLocaleLowerCase().trim();
+
+    //Filter items array to find the matches in the name or description
+    const filteredItems = items.filter((item) => {
+        return (
+            item.name.toLocaleLowerCase().includes(searchTerm) || 
+            item.description.toLocaleLowerCase().includes(searchTerm) 
+        );
+    });
+
+    // updates the result count display
+    resultsCount.textContent = `{fiteredItems.length}`
+}
